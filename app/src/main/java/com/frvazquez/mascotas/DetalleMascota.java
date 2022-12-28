200a54542b67c13a77a3a7b0183c57bd712d2527
frvazquez <crazzy-rock@live.com.mx>
2022-12-28 10:08:58 -0600

    se agregan las activitis listado y detalle mascota

---
+++ app/src/main/java/com/frvazquez/mascotas/DetalleMascota.java
@@ -0,0 +1,14 @@
+package com.frvazquez.mascotas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetalleMascota extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detalle_mascota);
+    }
+}
