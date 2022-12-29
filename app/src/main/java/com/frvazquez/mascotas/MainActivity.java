234b3db52fff12615e799e040185a0f8e752ab0a
frvazquez <crazzy-rock@live.com.mx>
2022-12-28 19:08:25 -0600

    Add Snackbar

--- app/src/main/java/com/frvazquez/mascotas/MainActivity.java
+++ app/src/main/java/com/frvazquez/mascotas/MainActivity.java
@@ -3,9 +3,12 @@ package com.frvazquez.mascotas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,7 +24,17 @@ public class MainActivity extends AppCompatActivity {
         miFAB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+              //  Toast.makeText(getBaseContext(), getResources().getString(R.string.mensaje), Toast.LENGTH_SHORT).show();
+                Snackbar
+                        .make(view, getResources().getString(R.string.mensaje), Snackbar.LENGTH_SHORT)
+                        .setAction(getResources().getString(R.string.texto_accion), new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                Log.i("SNACBAR", "Click en Snackbar");
+                            }
+                        })
+                        .setActionTextColor(getResources().getColor(R.color.colorRedShort))
+                        .show();
             }
         });
     }
